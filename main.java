515363a7bcbc1c06e4dfeef651b67bc4e77a1072
ManiKetabchi <Mketabchi@bayviewglen.ca>
2023-09-12 09:08:26 -0400

    Version 2

--- main.java
+++ main.java
@@ -6,8 +6,17 @@ public class main {
         System.out.println("C) Jupiter");
         System.out.println("D) Saturn");
         System.out.println("Correct Answer: C) Jupiter");
-        // System.out.println("Exploring Huron-Wendat Heritage");
-        // System.out.println("Aspects of Huron-Wendat Culture");
-        // System.out.println
+
+        System.out.println("");
+
+        System.out.println("Exploring Huron-Wendat Heritage");
+        System.out.println("Aspects of Huron-Wendat Culture");
+        System.out.println("- Traditional Practices");
+        System.out.println("- Art and Craftsmanship");
+        System.out.println("- Language and Storytelling");
+        System.out.println("- Community Traditions");
+        System.out.println("- Food and Medicine");
+        System.out.println("Date: Wednesday, November 15, 2023");
+        System.out.println("Duration: 60 minutes");
     }
 }
