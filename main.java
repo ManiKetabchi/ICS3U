e222f6d12f3c3d91765fe7afbbf82f94abcd087d
ManiKetabchi <Mketabchi@bayviewglen.ca>
2023-09-12 08:56:44 -0400

    Version1

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Question: What is the largest planet in our solar system?");
+        System.out.println("A) Earth");
+        System.out.println("B) Mars");
+        System.out.println("C) Jupiter");
+        System.out.println("D) Saturn");
+        System.out.println("Correct Answer: C) Jupiter");
+        // System.out.println("Exploring Huron-Wendat Heritage");
+        // System.out.println("Aspects of Huron-Wendat Culture");
+        // System.out.println
+    }
+}
